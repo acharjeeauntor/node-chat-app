053986a307f3bdad85a25ff4c2c4f6650ddf2ebf
Auntor <acharjeeauntor@gmail.com>
2019-02-01 22:49:33 +0600

    greet new user and aleart others

--- server/server.js
+++ server/server.js
@@ -18,6 +18,20 @@ io.on('connection',function(socket){
 console.log('new user connected');
 
 
+socket.emit('newMessage',{
+   from:'Admin',
+   text:'welcome to the chat app',
+   createAt:new Date().getTime()
+});
+
+socket.broadcast.emit('newMessage',{
+   from:'Admin',
+   text:'new user connected',
+   createAt:new Date().getTime()
+});
+
+
+
 socket.on('createMessage',function(message){
 console.log('Create Message:',message);
 io.emit('newMessage',{
@@ -25,10 +39,18 @@ io.emit('newMessage',{
    text:message.text,
    createAt:new Date().getTime()
 });
+
+// socket.broadcast.emit('newMessage',{
+//       from:message.from,
+//       text:message.text,
+//       createAt:new Date().getTime()
+// });
+
+
 });
 
 socket.on('disconnect',function(){
-   console.log('user was disconnectd');
+   console.log('user was disconnected');
 });
 });
 
